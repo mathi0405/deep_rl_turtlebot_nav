c765cbd7976427bbc95ed2a81bbe9647fbe43300
M.Mani chandan <108810456+mathi0405@users.noreply.github.com>
2026-06-28 13:30:34 +0200

    Refactor env_bridge.py for cleaner code structure

--- src/turtlebot3_smart_nav/scripts/env_bridge.py
+++ src/turtlebot3_smart_nav/scripts/env_bridge.py
@@ -17,77 +17,61 @@ class TurtleBot3NavigationEnv:
         self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
         self.state_service = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
         
-        self.action_space = [
-            [0.18,  0.0],   
-            [0.12,  0.75],  
-            [0.12, -0.75],  
-            [0.05,  1.5],   
-            [0.05, -1.5]    
-        ]
+        self.discrete_action_space = [[0.18, 0.0], [0.12, 0.75], [0.12, -0.75], [0.05, 1.5], [0.05, -1.5]]
 
     def get_lidar_state(self, scan_data):
-        raw_ranges = np.array(scan_data.ranges)
-        raw_ranges[raw_ranges == np.inf] = 3.5
-        
+        raw = np.array(scan_data.ranges)
+        raw[raw == np.inf] = 3.5
         downsampled = []
-        step = len(raw_ranges) // self.num_scan_samples
-        for i in range(0, len(raw_ranges), step):
-            sector = raw_ranges[i : i + step]
-            downsampled.append(np.min(sector))
-            
+        step = len(raw) // self.num_scan_samples
+        for i in range(0, len(raw), step):
+            downsampled.append(np.min(raw[i : i + step]))
         return np.array(downsampled[:self.num_scan_samples])
 
     def respawn_goal(self):
         rospy.wait_for_service('/gazebo/set_model_state')
-        new_state = ModelState()
-        new_state.model_name = 'target_goal'
-        new_state.pose.position.x = random.uniform(-2.0, 2.0)
-        new_state.pose.position.y = random.uniform(-2.0, 2.0)
-        new_state.pose.position.z = 0.0
-        try:
-            self.state_service(new_state)
-        except rospy.ServiceException:
-            pass
+        state = ModelState()
+        state.model_name = 'target_goal'
+        state.pose.position.x = random.uniform(-2.0, 2.0)
+        state.pose.position.y = random.uniform(-2.0, 2.0)
+        try: self.state_service(state)
+        except: pass
 
-    def step(self, action_index):
+    def step(self, action):
         rospy.wait_for_service('/gazebo/unpause_physics')
-        try:
-            self.unpause_sim()
-        except rospy.ServiceException:
-            pass
+        try: self.unpause_sim()
+        except: pass
             
         vel_cmd = Twist()
-        vel_cmd.linear.x = self.action_space[action_index][0]
-        vel_cmd.angular.z = self.action_space[action_index][1]
+        # Handle Continuous Arrays vs Discrete Ints
+        if isinstance(action, (list, np.ndarray, tuple)):
+            vel_cmd.linear.x = max(0.0, action[0] * 0.22) # Prevent reverse driving
+            vel_cmd.angular.z = action[1] * 2.0
+        else:
+            vel_cmd.linear.x = self.discrete_action_space[action][0]
+            vel_cmd.angular.z = self.discrete_action_space[action][1]
+            
         self.velocity_publisher.publish(vel_cmd)
         
         data = None
         while data is None and not rospy.is_shutdown():
-            try:
-                data = rospy.wait_for_message('/scan', LaserScan, timeout=0.5)
-            except:
-                pass
+            try: data = rospy.wait_for_message('/scan', LaserScan, timeout=0.5)
+            except: pass
                 
         rospy.wait_for_service('/gazebo/pause_physics')
-        try:
-            self.pause_sim()
-        except rospy.ServiceException:
-            pass
+        try: self.pause_sim()
+        except: pass
             
         state = self.get_lidar_state(data)
-        min_distance = np.min(state)
-        done = bool(min_distance < 0.18)
+        done = bool(np.min(state) < 0.18)
         reward = -25.0 if done else 0.1
         
         return state, reward, done
 
     def reset(self):
         rospy.wait_for_service('/gazebo/reset_simulation')
-        try:
-            self.reset_proxy()
-        except rospy.ServiceException:
-            pass
-            
+        try: self.reset_proxy()
+        except: pass
         self.respawn_goal()
         data = rospy.wait_for_message('/scan', LaserScan)
         return self.get_lidar_state(data)
