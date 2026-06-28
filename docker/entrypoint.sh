03e899b963f585ed62120f849e196b4e67ce5502
M.Mani chandan <108810456+mathi0405@users.noreply.github.com>
2026-06-28 13:26:54 +0200

    Refactor entrypoint.sh to streamline commands

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 set -e
-
-# Source standard ROS environment
 source /opt/ros/noetic/setup.bash
-
-# Build the workspace if it hasn't been built yet
 if [ ! -d "/root/catkin_ws/devel" ]; then
     echo "Building Catkin Workspace..."
-    cd /root/catkin_ws
-    catkin_make
+    cd /root/catkin_ws && catkin_make
 fi
-
-# Source the custom workspace
 source /root/catkin_ws/devel/setup.bash
-
-# Execute whatever command was passed to Docker
 exec "$@"
